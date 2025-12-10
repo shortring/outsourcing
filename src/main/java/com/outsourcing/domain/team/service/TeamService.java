2234f4aa02f9a019bee9d7445893a15c1242d314
4realavenue <4realdane@gmail.com>
2025-12-10 16:24:40 +0900

    feat: TeamMember 기능 추가
    팀에 멤버 추가 삭제 기능 추가

--- src/main/java/com/outsourcing/domain/team/service/TeamService.java
+++ src/main/java/com/outsourcing/domain/team/service/TeamService.java
@@ -1,21 +1,34 @@
 package com.outsourcing.domain.team.service;
 
 import com.outsourcing.common.entity.Team;
+import com.outsourcing.common.entity.TeamMember;
+import com.outsourcing.common.entity.User;
+import com.outsourcing.common.exception.CustomException;
+import com.outsourcing.common.exception.ErrorMessage;
 import com.outsourcing.domain.team.dto.request.CreateTeamRequestDto;
 import com.outsourcing.domain.team.dto.request.UpdateTeamRequestDto;
 import com.outsourcing.domain.team.dto.response.CreateTeamResponseDto;
 import com.outsourcing.domain.team.dto.response.GetDetailTeamResponseDto;
 import com.outsourcing.domain.team.dto.response.UpdateTeamResponseDto;
 import com.outsourcing.domain.team.repository.TeamRepository;
+import com.outsourcing.domain.teamMember.dto.request.TeamAddMemberRequest;
+import com.outsourcing.domain.teamMember.dto.response.TeamAddMemberResponse;
+import com.outsourcing.domain.teamMember.dto.response.TeamMemberResponseDto;
+import com.outsourcing.domain.teamMember.repository.TeamMemberRepository;
+import com.outsourcing.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class TeamService {
 
     private final TeamRepository teamRepository;
+    private final UserRepository userRepository;
+    private final TeamMemberRepository teamMemberRepository;
 
     // 팀 생성
     @Transactional
@@ -36,11 +49,48 @@ public class TeamService {
         return response;
     }
 
+    // 멤버 추가
+    @Transactional
+    public List<TeamAddMemberResponse> addMemberTeam (Long teamId, TeamAddMemberRequest request) {
+        Team findTeam = teamRepository.findById(teamId)
+                .orElseThrow(() -> new CustomException(ErrorMessage.NOT_FOUND_TEAM));
+
+        User findUser = userRepository.findById(request.getUserId())
+                .orElseThrow(() -> new CustomException(ErrorMessage.NOT_FOUND_USER));
+
+        if (teamMemberRepository.existsByTeamAndUser(findTeam, findUser)) {
+            throw new CustomException(ErrorMessage.CONFLICT_ALREADY_IN_TEAM);
+        }
+
+        TeamMember teamMember = new TeamMember(findTeam, findUser);
+
+        teamMemberRepository.save(teamMember);
+
+        List<TeamAddMemberResponse> response = findTeam.getMembers().stream()
+                .map(teamMembers -> new TeamAddMemberResponse(
+                        teamMembers.getUser().getId(),
+                        teamMembers.getUser().getUsername(),
+                        teamMembers.getUser().getName()
+                ))
+                .toList();
+
+        return response;
+    }
+
+    // 멤버 삭제
+    @Transactional
+    public void removeMemberTeam (Long teamId, Long userId) {
+        TeamMember teamMember = teamMemberRepository.findByTeamIdAndUserId(teamId, userId)
+                .orElseThrow(() -> new CustomException(ErrorMessage.NOT_FOUND_TEAM_MEMBER));
+
+        teamMemberRepository.delete(teamMember);
+    }
+
     // 팀 상세 조회 (팀의 정보와 팀에 속한 멤버 조회)
     @Transactional
     public GetDetailTeamResponseDto getDetailTeam (Long teamId) {
-        Team findTeam = teamRepository.findById(teamId).orElseThrow
-                ( () -> new IllegalArgumentException("존재하지 않는 팀입니다."));
+        Team findTeam = teamRepository.findById(teamId)
+                .orElseThrow(() -> new CustomException(ErrorMessage.NOT_FOUND_TEAM));
 
         GetDetailTeamResponseDto responseDto = new GetDetailTeamResponseDto(
                 findTeam.getId(),
@@ -48,7 +98,6 @@ public class TeamService {
                 findTeam.getDescription(),
                 findTeam.getCreatedAt()
         );
-
         return responseDto;
     }
 
