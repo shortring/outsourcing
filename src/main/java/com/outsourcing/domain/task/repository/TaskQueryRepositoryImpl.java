4fa032c74a28c563600c29f6669ffa591d7d10eb
Jinsoo <milestone9701@gmail.com>
2025-12-11 10:06:31 +0900

    fix: import 제거

--- src/main/java/com/outsourcing/domain/task/repository/TaskQueryRepositoryImpl.java
+++ src/main/java/com/outsourcing/domain/task/repository/TaskQueryRepositoryImpl.java
@@ -8,7 +8,6 @@ import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
