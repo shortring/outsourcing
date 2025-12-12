deb441d1bff3473dbc0de202eb9d64382e253ae0
chan <zinchan1@naver.com>
2025-12-12 19:35:57 +0900

    fix: 삭제된 쿼리문 다시 넣음

--- src/main/java/com/outsourcing/domain/team/repository/TeamRepository.java
+++ src/main/java/com/outsourcing/domain/team/repository/TeamRepository.java
@@ -2,8 +2,19 @@ package com.outsourcing.domain.team.repository;
 
 import com.outsourcing.common.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
+
+    @Query("""
+           SELECT t
+           FROM Team t
+           WHERE t.name LIKE %:query% OR t.description LIKE %:query%
+           """)
+    List<Team> searchByKeyword(@Param("query") String query);
 
     boolean existsByName(String name);
 
