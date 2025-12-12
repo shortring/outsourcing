fc6f3ce1b6b6a6b4aa9636fbb443310a18795951
KWON-WOO <ckw075@naver.com>
2025-12-12 21:49:16 +0900

    refactor: record 클래스였던TaskResponse 수정. AOP로직 일부 변경

--- src/main/java/com/outsourcing/domain/task/dto/response/TaskResponse.java
+++ src/main/java/com/outsourcing/domain/task/dto/response/TaskResponse.java
@@ -1,24 +1,28 @@
 package com.outsourcing.domain.task.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.outsourcing.common.entity.task.TaskPriority;
 import com.outsourcing.common.entity.task.TaskStatus;
 import com.outsourcing.domain.task.dto.TaskDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.time.Instant;
 
 // 값 : id, 타이틀, 세부사항, 스테이터스, 우선순위, 담당자, 베이스 엔티티 2종, DueDate.
-public record TaskResponse(
-        Long id,
-        String title,
-        String description,
-        TaskStatus status,
-        TaskPriority priority,
-        Long assigneeId,
-        AssigneeResponse assignee, // id, username
-        Instant createdAt,
-        Instant updatedAt,
-        Instant dueDate
-) {
+@Getter
+@AllArgsConstructor
+public class TaskResponse {
+    Long id;
+    String title;
+    String description;
+    TaskStatus status;
+    TaskPriority priority;
+    Long assigneeId;
+    AssigneeResponse assignee; // id, username
+    Instant createdAt;
+    Instant updatedAt;
+    Instant dueDate;
     public static TaskResponse from(TaskDto taskDto) {
         return new TaskResponse(
                 taskDto.getId(),
